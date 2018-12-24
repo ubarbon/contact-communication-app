e5bb685929459b8b4d5c0dda8c419ab849dbba20
Uriser Barbón Suárez <uriserbarbon@gmail.com>
2018-12-24 14:52:07 +0100

    Removed confirm message cmp

--- src/app/common/message/confirm-message-event.ts
+++
@@ -1,7 +0,0 @@
-import {ConfirmMessageHandler} from './confirm-message-handler';
-
-export interface ConfirmMessageEvent {
-    onBtnOkClick(confirmMessageHandler: ConfirmMessageHandler): void;
-
-    onBtnCancelClick(confirmMessageHandler: ConfirmMessageHandler): void;
-}
